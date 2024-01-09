6badc30e2bcd310e8423e53966b08466d032f1fe
thahir02 <155128623+thahir02@users.noreply.github.com>
2024-01-09 09:13:45 +0530

    Add files via upload

---
+++ CheckedException.java
@@ -0,0 +1,13 @@
+//predefined checked Exception. in this Exception is checked by compipler 
+import java.util.*;
+public class CheckedException{
+    public static void main(String arg[])throws InterruptedException
+    {
+        Scanner s=new Scanner(System.in);
+        System.out.println("java");
+        Thread.sleep(3000);//it relaxes for 3sec and continues execution
+        System.out.println("python");
+        
+        
+    }
+}
