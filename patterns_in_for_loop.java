f454d626d03812529f229caf84736debfd512269
SM Thahir <155128623+thahir02@users.noreply.github.com>
2026-08-07 12:48:54 +0530

    Add multiple pattern printing programs in Java

--- patterns_in_for_loop.java
+++ patterns_in_for_loop.java
@@ -16,6 +16,17 @@ public class p1 {
 		}
 	}		
 }
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+* * * * * * 
+*/
 //pattern 2 :-
 import java.util.Scanner;
 public class p2 {
@@ -33,6 +44,16 @@ public class p2 {
 		}
 	}		
 }
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+1 1 1 1 1 
+2 2 2 2 2 
+3 3 3 3 3 
+4 4 4 4 4 
+5 5 5 5 5 
+*/
 //pattern 3 :-
 import java.util.Scanner;
 public class p3 {
@@ -50,3 +71,186 @@ public class p3 {
 		}
 	}		
 }
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+*/
+import java.util.Scanner;
+public class p4 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		for(int n1=0;n1<=i;n1++) {
+			for(int n2=0;n2<=j;n2++) {
+				if(n1==0 || n1==i || n2==0 || n2==j ) {
+					System.out.print("*"+" ");
+				}
+				else {
+					System.out.print(" "+" ");
+				}
+			}
+			System.out.println();
+		}
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+* * * * * * 
+*         * 
+*         * 
+*         * 
+*         * 
+* * * * * * 
+*/
+import java.util.Scanner;
+public class p5 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		int count=1;
+		for(int n1=1;n1<=i;n1++) {
+			for(int n2=1;n2<=j;n2++) {
+				if(count<10) {
+					System.out.print(0);
+				}
+					System.out.print(count+" ");
+					count++;
+				}
+			System.out.println();
+		}
+}
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+01 02 03 04 05 
+06 07 08 09 10 
+11 12 13 14 15 
+16 17 18 19 20 
+21 22 23 24 25 
+*/	
+import java.util.Scanner;
+public class p6 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		for(int n1=1;n1<=i;n1++) {
+			for(int n2=1;n2<=j;n2++) {
+				if(n1*n2<10) {
+					System.out.print(0);
+				}
+					System.out.print(n1*n2+" ");
+				}
+			System.out.println();
+		}
+}
+}
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+01 02 03 04 05 
+02 04 06 08 10 
+03 06 09 12 15 
+04 08 12 16 20 
+05 10 15 20 25 
+*/	
+import java.util.Scanner;
+public class p7 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		int count;
+		for(int n1=0;n1<i;n1++) {
+			count=n1+1;
+			for(int n2=0;n2<j;n2++) {
+				if(count<10) {
+					System.out.print(" ");
+				}
+					System.out.print(count++ +" ");
+				}
+			System.out.println();
+		}
+}
+}
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+01 02 03 04 05 
+02 04 06 08 10 
+03 06 09 12 15 
+04 08 12 16 20 
+05 10 15 20 25 
+*/	
+import java.util.Scanner;
+public class p8 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		for(int n1=0;n1<i;n1++) {
+			for(int n2=0;n2<=j;n2++) { // you can remove if condition and use n2<=n1 based on same indexes
+				if(n1>=n2)
+					System.out.print("*"+" ");
+			}
+			System.out.println();
+		}
+}
+}
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/	
+import java.util.Scanner;
+public class p9 {
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.print("Enter the num1 : ");
+		int i=scanner.nextInt();
+		System.out.print("Enter the num2 : ");
+		int j=scanner.nextInt();
+		for(int n1=0;n1<i;n1++) {
+			for(int n2=0;n2<=n1;n2++) { 
+					System.out.print(n2+1 +" ");
+			}
+			System.out.println();
+		}
+}
+}	
+/*
+Output :-
+Enter the num1 : 5
+Enter the num2 : 5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5 
+*/	
